920e177c091e9e575da0ba902e2da07f9aec53fd
MUKARUGIRA Francine <francinerugira@.com>
2022-03-18 14:58:41 +0200

    adding files

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Flutterwave Payment</title>
+</head>
+<body>
+    <form id="" action="makepayment.php" method="post">
+        <input type="text" name="name" id="name" placeholder="Full Name">
+        <input type="email" name="email" id="email" placeholder="Email">
+        <input type="number" name="amount" id="amount" placeholder="Amount">
+        <input type="submit" value="Pay" name="pay">
+    </form>
+
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://checkout.flutterwave.com/v3.js"></script> -->
+
+    
+</body>
+</html>
